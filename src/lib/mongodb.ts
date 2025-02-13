73b85af750ecbea3fd3be6eb4788592006e95f4f
AJRoxas <alieljacob.roxas@mail.utoronto.ca>
2025-02-12 22:47:40 -0500

    From last commit

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -13,12 +13,10 @@ if (!cached) {
 }
 
 async function mongodb() {
-  const MONGODB_URI = process.env.MONGODB_URI!;
+  const uri = process.env.MONGODB_URI!;
 
-  if (!MONGODB_URI) {
-    throw new Error(
-      "Please define the MONGODB_URI environment variable inside .env.local",
-    );
+  if (!uri) {
+    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
   }
 
   if (cached.conn) {
@@ -28,7 +26,7 @@ async function mongodb() {
     const opts = {
       bufferCommands: false,
     };
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
       return mongoose;
     });
   }
