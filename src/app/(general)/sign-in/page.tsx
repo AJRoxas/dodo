cacd9d82eb1f544ff7cc4c541d4eed2decb8fab4
AJRoxas <alieljacob.roxas@mail.utoronto.ca>
2025-03-01 18:46:42 -0500

    Added Sign up and Sign in pages

---
+++ src/app/(general)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { SignInSection } from '@/components/sign-in-sign-up/Authentication'
+
+const SignInPage = () => {
+  return(
+    <main>
+      <SignInSection></SignInSection>
+    </main>
+  );
+};
+
+export default SignInPage;
---
+++ src/app/(general)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { SignUpSection } from "@/components/sign-in-sign-up/Authentication";
+
+const SignUpPage = () => {
+  return(
+    <main>
+      <SignUpSection></SignUpSection>
+    </main>
+  );
+};
+
+export default SignUpPage;
