763aa429075cd3b78612e9b40581bd4b3cf87c60
AJRoxas <alieljacob.roxas@mail.utoronto.ca>
2025-03-04 21:32:22 -0500

    Added all SSO methods

--- src/components/authentication/SignInSection.tsx
+++ src/components/authentication/SignInSection.tsx
@@ -4,41 +4,80 @@ import Button from '@/components/Button';
 import app from '@/lib/firebase/firebase';
 import { faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faMask } from '@fortawesome/free-solid-svg-icons';
-import { getAuth, signInAnonymously } from 'firebase/auth';
-import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import {
+  AuthProvider,
+  getAuth,
+  signInAnonymously,
+  signInWithPopup,
+} from 'firebase/auth';
 import { useRouter } from 'next/navigation';
-
-const redirectToDashboard = (router: AppRouterInstance) => {
-  router.push('/getting-started');
-};
-
-const handleAnonymousSignIn = async (router: AppRouterInstance) => {
-  try {
-    const credentials = await signInAnonymously(getAuth(app));
-    const idToken = await credentials.user.getIdToken();
-
-    await fetch("/api/login", {
-      headers: {
-        Authorization: `Bearer ${idToken}`,
-      },
-    });
-
-    redirectToDashboard(router);
-  } catch (error) {
-    console.log((error as Error).message);
-  }
-};
+import { GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
 
 const SignInSection = () => {
   const router = useRouter();
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+  gitHubProvider.setCustomParameters({
+    allow_signup: 'false',
+  });
+
+  const redirectToDashboard = () => {
+    router.push('/getting-started');
+  };
+
+  const handleSSO = async (provider: AuthProvider) => {
+    try {
+      const credentials = await signInWithPopup(getAuth(app), provider);
+      const idToken = await credentials.user.getIdToken();
+
+      await fetch('/api/login', {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      });
+
+      redirectToDashboard();
+    } catch (error) {
+      console.log((error as Error).message);
+    }
+  };
+
+  const handleAnonymousSignIn = async () => {
+    try {
+      const credentials = await signInAnonymously(getAuth(app));
+      const idToken = await credentials.user.getIdToken();
+
+      await fetch('/api/login', {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      });
+
+      redirectToDashboard();
+    } catch (error) {
+      console.log((error as Error).message);
+    }
+  };
 
   return (
     <>
       <div className="flex flex-col justify-center items-center gap-4 motion-safe:animate-fade-left">
-        <Button size="md" icon={faGoogle} onClick={() => {}}>
+        <Button
+          size="md"
+          icon={faGoogle}
+          onClick={() => {
+            handleSSO(googleProvider);
+          }}
+        >
           Continue with Google
         </Button>
-        <Button size="md" icon={faGithub} onClick={() => {}}>
+        <Button
+          size="md"
+          icon={faGithub}
+          onClick={() => {
+            handleSSO(gitHubProvider);
+          }}
+        >
           Continue with GitHub
         </Button>
       </div>
@@ -50,7 +89,7 @@ const SignInSection = () => {
           size="md"
           icon={faMask}
           onClick={() => {
-            handleAnonymousSignIn(router);
+            handleAnonymousSignIn();
           }}
         >
           Enter Anonymously
